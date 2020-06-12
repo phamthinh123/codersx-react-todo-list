5a7b035c84f2dfc57b01872aacd05becd922018e
thinh <thinoi987654321@gmail.com>
2020-06-12 16:54:12 +0700

    bai7

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TabMenu from "./components/TabMenu"
+import homeOutline from './img/500_F_97000769_R4zepLTgmf8G22W7G2o8JA1HeiVK2CkK.jpg';
+import giftIcon from './img/500_F_118730742_uNTl3r0A5uKvfPdjvSEDroehYYz8P5G9.jpg';
 
+
+import settingOutlined from './img/500_F_177304889_R7cxBSbOlwYk05BRuwj8VZxpLClKSzlk.jpg';
+
+import cafeIcon from './img/500_F_171267967_OZsnAMcnGyUYs9HbguT2kBbSkJOEIdfX.jpg';
+// npm install --save-dev @iconify/react @iconify/icons-cil
+
+import cloudUpload from './img/500_F_164165971_ELxPPwdwHYEhg4vZ3F4Ej7OmZVzqq4Ov.jpg';
 class App extends Component {
   constructor(){
   super();
   this.items=[
-  { icon: 'homeOutline', label: 'HOME' },
-  { icon: 'giftIcon', label: 'DEALS' },
-  { icon: 'cloudUpload', label: 'UPLOAD' },
-  { icon: 'cafeIcon', label: 'WORK' },
-  { icon: 'settingOutlined', label: 'SETTINGS' }
+  { icon: homeOutline, label: 'HOME' },
+  { icon: giftIcon, label: 'DEALS' },
+  { icon: cloudUpload, label: 'UPLOAD' },
+  { icon: cafeIcon, label: 'WORK' },
+  { icon: settingOutlined, label: 'SETTINGS' }
 ];
 }
   render() {
--- src/components/TabMenu.js
+++ src/components/TabMenu.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
-import { Icon, InlineIcon } from '@iconify/react';
-import homeOutline from '@iconify/icons-ion/home-outline';
-import giftIcon from '@iconify/icons-bytesize/gift';
 
 
-import settingOutlined from '@iconify/icons-ant-design/setting-outlined';
 
-import cafeIcon from '@iconify/icons-carbon/cafe';
-// npm install --save-dev @iconify/react @iconify/icons-cil
-
-import cloudUpload from '@iconify/icons-cil/cloud-upload';
 
 class TabMenu extends Component{
+
   render(){
 
     return(
     <div className="TabMenu">
-      <div><Icon icon={this.props.item.icon}  width="65" height="65"/>{this.props.item.label}</div>
-
+      <div><img src={this.props.item.icon}  width="65" height="65"/>{this.props.item.label}</div>
+  
       </div>
     )
   }
